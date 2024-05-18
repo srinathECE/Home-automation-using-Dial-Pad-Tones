b598eed07f8fc6d7490038945c6daefe265aeb55
SRINATH S A <115480750+srinathECE@users.noreply.github.com>
2024-05-18 10:09:28 +0530

    Update dtmfproject.ino

--- dtmfproject.ino
+++ dtmfproject.ino
@@ -1,50 +1,47 @@
+# Import the required library
+import RPi.GPIO as GPIO
+import time
 
+# Set GPIO pin numbers
+rfbuttonPin1 = 2
+rfbuttonPin2 = 3
+ledPin = 5
+buzzerpin = 4
 
-// constants won't change. They're used here to set pin numbers:
-const int rfbuttonPin1 = 2;  // the number of the pushbutton pin
-const int rfbuttonPin2 = 3;
-const int ledPin = 5;    // the number of the LED pin
-const int buzzerpin = 4;
-// variables will change:
- int buttonState1 = 0;  // variable for reading the pushbutton status
-int buttonState2 = 0;
+# Set GPIO mode
+GPIO.setmode(GPIO.BCM)
 
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzerpin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(rfbuttonPin1, INPUT);
-  pinMode(rfbuttonPin2, INPUT);
-}
+# Set GPIO pin directions
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buzzerpin, GPIO.OUT)
+GPIO.setup(rfbuttonPin1, GPIO.IN)
+GPIO.setup(rfbuttonPin2, GPIO.IN)
 
-void loop() 
-{
-  // read the state of the pushbutton value:
-  
-  buttonState1 = digitalRead(rfbuttonPin1);
+try:
+    while True:
+        # Read the state of the pushbuttons
+        buttonState1 = GPIO.input(rfbuttonPin1)
+        buttonState2 = GPIO.input(rfbuttonPin2)
 
-  buttonState2 = digitalRead(rfbuttonPin2);
+        # Check if the first pushbutton is pressed
+        if buttonState1 == GPIO.HIGH:
+            # Turn on the LED
+            GPIO.output(ledPin, GPIO.HIGH)
+        else:
+            # Turn off the LED
+            GPIO.output(ledPin, GPIO.LOW)
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState1 == HIGH) 
-  {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } 
-  else 
-  {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
+        # Check if the second pushbutton is pressed
+        if buttonState2 == GPIO.HIGH:
+            # Turn on the buzzer
+            GPIO.output(buzzerpin, GPIO.HIGH)
+        else:
+            # Turn off the buzzer
+            GPIO.output(buzzerpin, GPIO.LOW)
 
-  if (buttonState2 == HIGH)
-   {
-    // turn buzzer on:
-    digitalWrite(buzzerpin, HIGH);
-  } else 
-  {
-    // turn buzzer off:
-    digitalWrite(buzzerpin, LOW);
-  }
-}
+        # Delay for a short period to avoid excessive CPU usage
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    # Clean up GPIO settings before exiting
+    GPIO.cleanup()
